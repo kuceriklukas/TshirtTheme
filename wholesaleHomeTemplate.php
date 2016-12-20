334913d64420c2a055ca205d193acd696a692ab8
adriano <mandolini1@gmail.com>
2016-12-20 19:08:12 +0100

    wholesale
    Wholesale home html and css. Will do detail page

---
+++ wholesaleHomeTemplate.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" media="all" href="style.css"/>
+    </head>
+    <body>
+        
+            
+            <div class="bodyWrap">
+                
+                <div class="header">
+                    <img src="images/callblue.png"> 
+                    <ul>
+                        <li>Home</li>
+                        <li>Tshirts</li>
+                        <li>Hats</li>
+                        <li>Bags</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                        
+                    </ul>
+                </div>
+                <div class="body">
+                    <div class="infoimg">
+                        <img src="images/ABanner1B.jpg">
+                    </div>
+                    <div class="items" id="ts">
+                        <div class="itemsPhoto">
+                            
+                        </div>
+                        <div class="itemsDescription">
+                            <h1>Tshirts</h1>
+                            <p>Many in all sizes and all colours</p>
+                        </div>
+                    </div>
+                    <div class="items" id="hats">
+                        <div class="itemsPhoto">
+                            
+                        </div>
+                        <div class="itemsDescription">
+                            <h1>Tshirts</h1>
+                            <p>Many in all sizes and all colours</p>
+                        </div>
+                    </div>
+                    <div class="items" id="sweats">
+                        <div class="itemsPhoto">
+                            
+                        </div>
+                        <div class="itemsDescription">
+                            <h1>Tshirts</h1>
+                            <p>Many in all sizes and all colours</p>
+                        </div>
+                    </div>
+                    <div class="items" id="others">
+                        <div class="itemsPhoto">
+                            
+                        </div>
+                        <div class="itemsDescription">
+                            <h1>Tshirts</h1>
+                            <p>Many in all sizes and all colours</p>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="Wfooter">
+                <div class="WfooterLogo">
+                    
+                </div>
+                <div class="WfooterDetails">
+                    <h1>
+                    contact
+                    </h1>
+                    <p>address1</p>
+                    <p>address2</p>
+                    <p>address3</p>
+                    <p>address4</p>
+                    <p>email</p>
+                    <p>telephone</p>
+                </div>
+                
+            </div>
+            
+        
+    </body>
+</html>
