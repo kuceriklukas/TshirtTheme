5955babec5bf4f84fdc7bdfd519c79ad4164c6aa
adriano <mandolini1@gmail.com>
2016-12-21 20:40:33 +0100

    wholesale
    wholesale detail and updated wholesale css

---
+++ wholesaleDetailTemplate.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" media="all" href="style.css"/>
+    </head>
+    <body>
+        <div class="bodyWrap">
+            <div class="header">
+                    <img src="images/callblue.png"> 
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="detailWhole.html">Tshirts</a></li>
+                        <li>Hats</li>
+                        <li>Bags</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                        
+                    </ul>
+                </div>
+            <div class="details1">
+                <div>
+                    <img src="images/blkBlank.jpg">
+                </div>
+                <h1>Available Colours</h1>
+                <div class="availableColours">
+                    
+                    <div class="cols"></div>
+                    <div class="cols"></div>
+                    <div class="cols"></div>
+                    <div class="cols"></div>
+                    <div class="cols"></div>
+                    <div class="cols"></div>
+                    <div class="cols"></div>
+                    <div class="cols"></div>
+                    <div class="cols"></div>
+                    <div class="cols"></div>
+                </div>
+            </div>
+            <div class="details2">
+                <h1>Title</h1>
+                <p>Description.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna 
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit 
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.</p>
+               
+                
+                <h1>Available sizes</h1>
+                <ul>
+                    <li>XS</li>
+                    <li>S</li>
+                    <li>M</li>
+                    <li>L</li>
+                    <li>XL</li>
+                    <li>XXL</li>
+                </ul>
+            </div>
+        </div>
+    </body>
+</html>
